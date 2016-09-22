6f157f88b6fb3e7814fbac75214381d9d3af775e
Victor Andrade de Almeida <victor.almeida@ccc.ufcg.edu.br>
2016-09-22 10:44:08 -0300

    Tela de Login e Signup funcionando

--- app/src/main/java/com/es/codinghub/app/LoginActivity.java
+++ app/src/main/java/com/es/codinghub/app/LoginActivity.java
@@ -2,13 +2,30 @@ package com.es.codinghub.app;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -21,6 +38,8 @@ public class LoginActivity extends Activity {
     @BindView(R.id.loginButton) Button confirmButton;
     @BindView(R.id.signupTextView) TextView signupButton;
 
+    private ProgressDialog progressDialog;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,27 +47,94 @@ public class LoginActivity extends Activity {
         setContentView(R.layout.login);
         ButterKnife.bind(this);
 
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setCancelable(false);
+        progressDialog.setMessage(getString(R.string.authenticating));
+
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        progressDialog.dismiss();
+    }
+
     @OnClick(R.id.signupTextView) void signup() {
-		Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+		Intent intent = new Intent(this, SignupActivity.class);
 		startActivity(intent);
+        finish();
     }
 
     @OnClick(R.id.loginButton) void login() {
     	if(validate() == false)
 			return;
 
-		ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this);
-        progressDialog.setIndeterminate(true);
-        progressDialog.setCancelable(false);
-        progressDialog.setMessage(getString(R.string.authenticating));
         progressDialog.show();
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://10.0.2.2:8080/login";
+
+        queue.add(new JsonObjectRequest(Request.Method.GET, url,
+
+            new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    try {
+                        Long userid = response.getLong("userid");
+                        onSuccess(userid);
+                    }
+
+                    catch (JSONException e) {
+                        onFail();
+                    }
+                }
+            },
+
+            new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    onFail();
+                }
+            })
+        {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+
+                String email = emailInput.getText().toString();
+                String password = passwordInput.getText().toString();
+
+                params.put("email", email);
+                params.put("password", password);
+
+                return params;
+            }
+        });
     }
 
-    public void onLoginFailed() {
-        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+    public void onSuccess(Long userid) {
+        SharedPreferences authPref = getSharedPreferences(
+                getString(R.string.authentication_file), Context.MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = authPref.edit();
+        editor.putLong("userid", userid);
+        editor.commit();
+
+        /* TODO ir para a tela principal
+        Intent intent = new Intent(this, HubActivity.class);
+        startActivity(intent); */
+
+        finish();
+    }
+
+    public void onFail() {
+        Toast.makeText(getBaseContext(), getString(R.string.authentication_failed),
+                Toast.LENGTH_LONG).show();
+
+        passwordInput.setText(null);
+        progressDialog.hide();
     }
 
     public boolean validate() {
@@ -57,10 +143,10 @@ public class LoginActivity extends Activity {
         String email = emailInput.getText().toString();
         String password = passwordInput.getText().toString();
 
-        if (android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches() == false)
+        if(android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches() == false)
         	emailInput.setError(getString(R.string.invalid_email));
 
-        else if (password.length() < 4)
+        else if(password.length() < 6)
         	passwordInput.setError(getString(R.string.short_password));
 
         else {
--- app/src/main/java/com/es/codinghub/app/SignupActivity.java
+++ app/src/main/java/com/es/codinghub/app/SignupActivity.java
@@ -2,12 +2,31 @@ package com.es.codinghub.app;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -20,6 +39,8 @@ public class SignupActivity extends Activity {
     @BindView(R.id.passwordAgainEditText) EditText passwordAgainInput;
     @BindView(R.id.signupButton) Button confirmButton;
 
+    private ProgressDialog progressDialog;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,22 +48,84 @@ public class SignupActivity extends Activity {
         setContentView(R.layout.signup);
         ButterKnife.bind(this);
 
+        progressDialog = new ProgressDialog(SignupActivity.this);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setCancelable(false);
+        progressDialog.setMessage(getString(R.string.signing_up));
+
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        progressDialog.dismiss();
     }
 
     @OnClick(R.id.signupButton) void signup() {
         if(validate() == false)
             return;
 
-        ProgressDialog progressDialog = new ProgressDialog(SignupActivity.this);
-        progressDialog.setIndeterminate(true);
-        progressDialog.setCancelable(false);
-        progressDialog.setMessage(getString(R.string.signing_up));
         progressDialog.show();
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://10.0.2.2:8080/user";
+
+        queue.add(new StringRequest(Request.Method.POST, url,
+
+            new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    onSuccess();
+                }
+            },
+
+            new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    onFail();
+                }
+            })
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+
+                String email = emailInput.getText().toString();
+                String password = passwordInput.getText().toString();
+
+                params.put("email", email);
+                params.put("password", password);
+
+                return params;
+            }
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = new HashMap<>();
+                return headers;
+            }
+        });
     }
 
-    public void onLoginFailed() {
-        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void onSuccess() {
+        onBackPressed();
+    }
+
+    public void onFail() {
+        Toast.makeText(getBaseContext(), getString(R.string.signup_failed),
+                Toast.LENGTH_LONG).show();
+
+        passwordInput.setText(null);
+        passwordAgainInput.setText(null);
+        progressDialog.hide();
     }
 
     public boolean validate() {
@@ -55,7 +138,7 @@ public class SignupActivity extends Activity {
         if (android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches() == false)
         	emailInput.setError(getString(R.string.invalid_email));
 
-        else if (password.length() < 4)
+        else if (password.length() < 6)
         	passwordInput.setError(getString(R.string.short_password));
 
         else if (password.equals(passwordAgain) == false) {
